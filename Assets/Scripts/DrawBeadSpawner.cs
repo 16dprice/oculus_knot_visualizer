639aa6e71206f31f435388c0c4cc56626bd6cfb0
Nathan Gillispie <nathaniel.gillispie972@topper.wku.edu>
2021-02-05 16:27:00 -0600

    Removed Unnecessary files. Added bead spawner.
    Hit RTrigger to make bead. Hit B to delete all of them

---
+++ Assets/Scripts/DrawBeadSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBeadSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _beadPrefab;
+    private List<GameObject> _beadPrefabObjects = new List<GameObject>();
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            var bead = Instantiate(_beadPrefab, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch), Quaternion.identity);
+            _beadPrefabObjects.Add(bead);
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            foreach (var gameObject in _beadPrefabObjects)
+            {
+                Destroy(gameObject);
+            }
+
+            _beadPrefabObjects.Clear();
+        }
+    }
+}
