eacf0cd431a94d4c6b0bec7f9c42946d71e65aad
Nathan Gillispie <nathaniel.gillispie972@topper.wku.edu>
2021-02-11 16:12:53 -0600

    Drawing knots works
    yay!

--- Assets/Scripts/DrawBeadSpawner.cs
+++ Assets/Scripts/DrawBeadSpawner.cs
@@ -4,25 +4,108 @@ using UnityEngine;
 
 public class DrawBeadSpawner : MonoBehaviour
 {
+    float radius = 0.01f;
+    int sides = 6;
+    
     [SerializeField] private GameObject _beadPrefab;
     private List<GameObject> _beadPrefabObjects = new List<GameObject>();
+    private List<Vector3> knot = new List<Vector3>();
+
+    private bool _previousTriggerState = false;
+    private bool _currentTriggerState = false;
+    private bool _drawingState = false;
+    
+    private Vector3 _previousTouchPos = Vector3.zero;
+    private Vector3 _currentTouchPos = Vector3.zero;
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        _currentTriggerState = OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger);
+        _currentTouchPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+
+        if (_currentTriggerState)
         {
-            var bead = Instantiate(_beadPrefab, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch), Quaternion.identity);
-            _beadPrefabObjects.Add(bead);
+            if (!_previousTriggerState)
+            {
+                _drawingState = true;
+            }
+        }
+        else
+        {
+            _drawingState = false;
+            _previousTouchPos = Vector3.zero;
+        }
+
+        if (_drawingState)
+        {
+            var d = _currentTouchPos - _previousTouchPos;
+            //Avoids square root calculation
+            if (d.x*d.x + d.y*d.y + d.z*d.z > 0.001f)
+            {
+                var bead = Instantiate(_beadPrefab, _currentTouchPos, Quaternion.identity);
+                _beadPrefabObjects.Add(bead);
+                knot.Add(_currentTouchPos);
+                
+                _previousTouchPos = _currentTouchPos;
+            }
         }
 
         if (OVRInput.GetDown(OVRInput.Button.Two))
-        {
-            foreach (var gameObject in _beadPrefabObjects)
-            {
-                Destroy(gameObject);
-            }
+            DestroyBeads();
 
-            _beadPrefabObjects.Clear();
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            DestroyKnot();
+            DisplayKnot();
+            
+            knot.Clear();
+        }
+
+        _previousTriggerState = _currentTriggerState;
+    }
+    
+    void DisplayKnot()
+    {
+        //TODO: check if this is necessary
+        if (knot.Count < 3) return;
+        
+        var beadsProvider = new DrawBeadsProvider(knot);
+        var stickModel = new LinkStickModel(beadsProvider);
+
+        var knotMeshObjects = stickModel.GetKnotMeshObjects(sides, radius);
+
+        foreach (var meshObject in knotMeshObjects)
+        {
+            if (meshObject != null)
+            {
+                meshObject.transform.parent = transform;
+                ResetTransform(meshObject);
+            }
+        }
+    }
+
+    void ResetTransform(GameObject obj)
+    {
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.rotation = Quaternion.identity;
+        obj.transform.localScale = Vector3.one;
+    }
+
+    void DestroyBeads()
+    {
+        foreach (var gameObject in _beadPrefabObjects)
+        {
+            Destroy(gameObject);
+        }
+
+        _beadPrefabObjects.Clear();
+    }
+
+    void DestroyKnot()
+    {
+        foreach (Transform child in this.transform)
+        {
+            Destroy(child.gameObject);
         }
     }
 }
---
+++ Assets/Scripts/DrawBeadsProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBeadsProvider: ILinkBeadsProvider
+{
+    public Vector3[] Knot;
+
+    public DrawBeadsProvider(List<Vector3> k)
+    {
+        Knot = k.ToArray();
+    }
+
+    public List<Vector3[]> GetBeadsList()
+    {
+        return new List<Vector3[]>(){Knot};
+    }
+}
