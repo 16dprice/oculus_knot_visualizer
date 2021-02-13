637083f23459404f07a9bcb1c404ac1641a809fe
DJ <donald.price077@topper.wku.edu>
2021-02-13 11:56:25 -0600

    refactor test file and add test cases to json file

--- Assets/Tests/PDCodeGeneratorTests/PDCodeGeneratorTests.cs
+++ Assets/Tests/PDCodeGeneratorTests/PDCodeGeneratorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
 using PDCodeGeneration;
 using UnityEngine;
@@ -14,20 +15,29 @@ namespace Tests
         {
             public TestCase(int crossingNumber, int ordering, int numComponents = 1)
             {
-                this.crossingNumber = crossingNumber;
-                this.ordering = ordering;
-                this.numComponents = numComponents;
+                CrossingNumber = crossingNumber;
+                Ordering = ordering;
+                NumComponents = numComponents;
             }
 
-            public int crossingNumber { get; }
-            public int ordering { get; }
-            public int numComponents { get; }
+            public int CrossingNumber { get; }
+            public int Ordering { get; }
+            public int NumComponents { get; }
         }
 
-        public List<TestCase> GetTestCases()
+        public List<TestCase> GetPDListTestCases()
         {
             var testCases = new List<TestCase>();
 
+            List<TestCase> items;
+            using (StreamReader r = new StreamReader("Assets/Tests/PDCodeGeneratorTests/pdCodeGeneratorTestCases.json"))
+            { 
+                var json = r.ReadToEnd();
+                items = JsonUtility.FromJson<List<TestCase>>(json);
+            }
+
+            Console.WriteLine(items);
+            
             testCases.Add(new TestCase(3, 1));
             testCases.Add(new TestCase(4, 1));
             testCases.Add(new TestCase(5, 1));
@@ -37,22 +47,22 @@ namespace Tests
         }
 
         [Test]
-        public void NewTestScriptSimplePasses()
+        public void TestGetPDListCount()
         {
-            foreach (var testCase in GetTestCases())
+            foreach (var testCase in GetPDListTestCases())
             {
                 var generator = new PDCodeGenerator(
                     new DefaultFileBeadsProvider(
-                        testCase.crossingNumber,
-                        testCase.ordering,
-                        testCase.numComponents
+                        testCase.CrossingNumber,
+                        testCase.Ordering,
+                        testCase.NumComponents
                     )
                 );
                 var crossingList = generator.GetPDList();
 
                 Assert.That(
                     crossingList.Count,
-                    Is.EqualTo(testCase.crossingNumber)
+                    Is.EqualTo(testCase.CrossingNumber)
                 );
             }
         }
