78d36b05027b4137cb271ee792243010e45758ff
Nathan Gillispie <nathaniel.gillispie972@topper.wku.edu>
2021-04-20 01:37:30 -0500

    strand passage framework
    math still needs to be implemented.

---
+++ Assets/Scripts/BeadsProviders/StrandPassProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace BeadsProviders
+{
+    public class StrandPassProvider : ILinkBeadsProvider
+    {
+        private List<LinkComponent> _linkComponents = null;
+        
+        //(int, int) tuple represents the component and segment number of the strands being passed
+        public StrandPassProvider(List<LinkComponent> linkComponents, (int, int) FirstStrandSegment, (int, int) SecondStrandSegment)
+        {
+
+            _linkComponents = linkComponents;
+        }
+
+        public List<LinkComponent> GetLinkComponents()
+        {
+            return _linkComponents;
+        }
+    }
+}
---
+++ Assets/Scripts/StrandPassage/StrandPassObject.cs
@@ -0,0 +1,56 @@
+﻿using BeadsProviders;
+using UI;
+using UnityEngine;
+
+namespace StrandPassage
+{
+    public class StrandPassObject : MonoBehaviour
+    {
+        private float radius = 0.5f;
+        private int sides = 6;
+    
+        private int NumComponents = 1;
+        private int CrossingNumber = 3;
+        private int Ordering = 1;
+
+        [SerializeField] int FirstStrandComponent = 0;
+        [SerializeField] int SecondStrandComponent = 0;
+        [SerializeField] int FirstStrandSegment = 0;
+        [SerializeField] int SecondStrandSegment = 0;
+
+        private int _previousFirstStrandSegment = 0;
+        private int _previousSecondStrandSegment = 0;
+        private int _previousFirstStrandComponent = 0;
+        private int _previousSecondStrandComponent = 0;
+        void Start()
+        {
+            var beadsProvider = new DefaultFileBeadsProvider(CrossingNumber, Ordering, NumComponents);
+            var linkStickModel = new LinkStickModel(beadsProvider);
+            
+            MeshManipulation.DisplayLink(transform, linkStickModel, sides, radius);
+        }
+
+        private void Update()
+        {
+            if (
+                _previousFirstStrandSegment != FirstStrandSegment ||
+                _previousSecondStrandSegment != SecondStrandSegment ||
+                _previousFirstStrandComponent != FirstStrandComponent ||
+                _previousSecondStrandComponent != SecondStrandComponent
+                )
+            {
+                var beadsProvider = new DefaultFileBeadsProvider(CrossingNumber, Ordering, NumComponents);
+                var strandPassProvider = new StrandPassProvider(beadsProvider.GetLinkComponents(),
+                    (FirstStrandComponent, FirstStrandSegment), (SecondStrandComponent, SecondStrandSegment));
+                var linkStickModel = new LinkStickModel(strandPassProvider);
+            
+                MeshManipulation.DisplayLink(transform, linkStickModel, sides, radius);
+                
+                _previousFirstStrandSegment = FirstStrandSegment;
+                _previousSecondStrandSegment = SecondStrandSegment;
+                _previousFirstStrandComponent = FirstStrandComponent;
+                _previousSecondStrandComponent = SecondStrandComponent;
+            }
+        }
+    }   
+}
