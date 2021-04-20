ec21f78888799b522537b78e44b657ac6ba81904
Nathan Gillispie <nathaniel.gillispie972@topper.wku.edu>
2021-04-20 02:55:55 -0500

    stuck on the math

--- Assets/Scripts/StrandPassage/StrandPassObject.cs
+++ Assets/Scripts/StrandPassage/StrandPassObject.cs
@@ -17,11 +17,11 @@ namespace StrandPassage
         [SerializeField] int SecondStrandComponent = 0;
         [SerializeField] int FirstStrandSegment = 0;
         [SerializeField] int SecondStrandSegment = 0;
-
-        private int _previousFirstStrandSegment = 0;
-        private int _previousSecondStrandSegment = 0;
+        
         private int _previousFirstStrandComponent = 0;
         private int _previousSecondStrandComponent = 0;
+        private int _previousFirstStrandSegment = 0;
+        private int _previousSecondStrandSegment = 0;
         void Start()
         {
             var beadsProvider = new DefaultFileBeadsProvider(CrossingNumber, Ordering, NumComponents);
@@ -33,12 +33,14 @@ namespace StrandPassage
         private void Update()
         {
             if (
-                _previousFirstStrandSegment != FirstStrandSegment ||
-                _previousSecondStrandSegment != SecondStrandSegment ||
                 _previousFirstStrandComponent != FirstStrandComponent ||
-                _previousSecondStrandComponent != SecondStrandComponent
+                _previousSecondStrandComponent != SecondStrandComponent ||
+                _previousFirstStrandSegment != FirstStrandSegment ||
+                _previousSecondStrandSegment != SecondStrandSegment
                 )
             {
+                
+                
                 var beadsProvider = new DefaultFileBeadsProvider(CrossingNumber, Ordering, NumComponents);
                 var strandPassProvider = new StrandPassProvider(beadsProvider.GetLinkComponents(),
                     (FirstStrandComponent, FirstStrandSegment), (SecondStrandComponent, SecondStrandSegment));
@@ -46,10 +48,10 @@ namespace StrandPassage
             
                 MeshManipulation.DisplayLink(transform, linkStickModel, sides, radius);
                 
-                _previousFirstStrandSegment = FirstStrandSegment;
-                _previousSecondStrandSegment = SecondStrandSegment;
                 _previousFirstStrandComponent = FirstStrandComponent;
                 _previousSecondStrandComponent = SecondStrandComponent;
+                _previousFirstStrandSegment = FirstStrandSegment;
+                _previousSecondStrandSegment = SecondStrandSegment;
             }
         }
     }   
---
+++ Assets/Scripts/StrandPassage/StrandPassProvider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Domain;
+using BeadsProviders;
+using UnityEngine;
+
+namespace StrandPassage
+{
+    public class StrandPassProvider : ILinkBeadsProvider
+    {
+        private List<LinkComponent> _linkComponents = null;
+        
+        //(int, int) tuple represents the component and segment number of the strands being passed
+        public StrandPassProvider(List<LinkComponent> linkComponents, (int, int) firstStrandSegment, (int, int) secondStrandSegment)
+        {
+            int componentNumber = firstStrandSegment.Item1 % linkComponents.Count;
+            firstStrandSegment = (componentNumber,
+                (firstStrandSegment.Item2 + 1) % linkComponents[componentNumber].BeadList.Count);
+            
+            componentNumber = secondStrandSegment.Item1 % linkComponents.Count;
+            secondStrandSegment = (componentNumber,
+                (secondStrandSegment.Item2 + 1) % linkComponents[componentNumber].BeadList.Count);
+            
+            
+            if (firstStrandSegment.Item1 == secondStrandSegment.Item1)
+            {
+                if (firstStrandSegment.Item2 != secondStrandSegment.Item2)
+                {
+                    var component = firstStrandSegment.Item1;
+                    
+                    var segmentBeads = linkComponents[component].BeadList;
+                    segmentBeads.Insert(firstStrandSegment.Item2, new Bead(Vector3.zero));
+                    segmentBeads.Insert(secondStrandSegment.Item2, new Bead(Vector3.zero));
+                    
+                    linkComponents[component] = new LinkComponent(segmentBeads);
+                }
+            }
+            else
+            {
+                var firstSegmentBeads = linkComponents[firstStrandSegment.Item1].BeadList;
+                var secondSegmentBeads = linkComponents[secondStrandSegment.Item2].BeadList;
+                            
+                firstSegmentBeads.Insert(firstStrandSegment.Item2, new Bead(Vector3.zero));
+                secondSegmentBeads.Insert(secondStrandSegment.Item2, new Bead(Vector3.zero));
+
+                linkComponents[firstStrandSegment.Item1] = new LinkComponent(firstSegmentBeads);
+                linkComponents[secondStrandSegment.Item1] = new LinkComponent(secondSegmentBeads);
+            }
+            
+
+            _linkComponents = linkComponents;
+        }
+
+        public List<LinkComponent> GetLinkComponents()
+        {
+            return _linkComponents;
+        }
+    }
+}
